27becbb01cb5c8e988e9f39af2dde3899bb05225
Andrius Cimermonas <Andrius.Cimermonas@bdo.global>
2021-03-01 14:21:28 +0200

    fixes

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,83 +1,69 @@
-import { useState } from 'react';
-import { useUserLogin } from '../lib/hooks';
-import { Spin } from 'antd';
+import { useForm } from 'react-hook-form';
 
-interface Credentials {
+interface LoginData {
   email: string;
   password: string;
 }
-
 const LoginForm: React.FC = () => {
-  const [creds, setCreds] = useState<Credentials>({ email: '', password: '' });
-  const { errors, loading, login } = useUserLogin();
-
-  const handleForm = (e) => {
-    e.preventDefault();
-    login(creds.email, creds.password);
+  const { register, errors, handleSubmit } = useForm();
+  const onSubmit = (data: LoginData) => {
+    console.log(data);
   };
-
   return (
-    <div
-      style={{ backgroundImage: 'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)' }}
-      className="h-screen font-sans py-20 login bg-cover"
-    >
-      <div className="bg-white lg:w-4/12 md:6/12 w-10/12 m-auto  shadow-2xl">
-        <Spin spinning={loading}>
-          <div className="py-8 px-8 rounded-xl">
-            <h1 className="font-medium text-2xl mt-3 text-center">Login</h1>
-            <form onSubmit={(e) => handleForm(e)}>
-              <div className="my-5 text-sm">
-                <label className="block text-black">Email</label>
-                <input
-                  value={creds.email}
-                  type="text"
-                  id="email"
-                  className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full"
-                  placeholder="Email"
-                  onChange={(e) => setCreds((prevState) => ({ ...prevState, email: e.target.value }))}
-                />
-              </div>
-              <div className="my-5 text-sm">
-                <label className="block text-black">Password</label>
-                <input
-                  value={creds.password}
-                  type="password"
-                  id="password"
-                  className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full"
-                  placeholder="Password"
-                  onChange={(e) => setCreds((prevState) => ({ ...prevState, password: e.target.value }))}
-                />
-                <span className="flex items-center font-medium tracking-wide text-red-500 text-xs my-2 ml-1">
-                  {errors}
-                </span>
-              </div>
-
-              <button
-                type="submit"
-                className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full"
-              >
-                Login
-              </button>
-              <div className="flex md:justify-between justify-center items-center mt-10">
-                <div style={{ height: 1 }} className="bg-gray-300 md:block hidden w-4/12"></div>
-                <p className="md:mx-2 text-sm font-light text-gray-400"> Beer Me! </p>
-                <div style={{ height: 1 }} className="bg-gray-300 md:block hidden w-4/12"></div>
-              </div>
-
-              <p className="mt-12 text-xs text-center font-light text-gray-400">
-                {' '}
-                Don't have an account?{' '}
-                <a href="../auth/register.html" className="text-black font-medium">
-                  {' '}
-                  Ask for one{' '}
-                </a>{' '}
-              </p>
-            </form>
-          </div>
-        </Spin>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="rounded-md">
+        <label htmlFor="email" className="block text-sm font-medium leading-5 text-gray-700">
+          Email address
+        </label>
+        <div className="mt-1 rounded-md">
+          <input
+            id="email"
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 shadow-sm"
+            type="email"
+            name="email"
+            ref={register({
+              required: 'Please enter an email',
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: 'Not a valid email'
+              }
+            })}
+          />
+          {errors.email && <div className="mt-2 text-xs text-red-600">{errors.email.message}</div>}
+        </div>
       </div>
-    </div>
+      <div className="mt-4">
+        <label htmlFor="password" className="block text-sm font-medium leading-5 text-gray-700">
+          Password
+        </label>
+        <div className="mt-1 rounded-md">
+          <input
+            id="password"
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 shadow-sm"
+            type="password"
+            name="password"
+            ref={register({
+              required: 'Please enter a password',
+              minLength: {
+                value: 6,
+                message: 'Should have at least 6 characters'
+              }
+            })}
+          />
+          {errors.password && <div className="mt-2 text-xs text-red-600">{errors.password.message}</div>}
+        </div>
+      </div>
+      <div className="mt-4">
+        <span className="block w-full rounded-md shadow-sm">
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+          >
+            Log in
+          </button>
+        </span>
+      </div>
+    </form>
   );
 };
-
 export default LoginForm;
