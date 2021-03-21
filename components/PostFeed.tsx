dceb940e55a26639d5e5aac603bfde7e799e48ec
Andrius Cimermonas <Andrius.Cimermonas@bdo.global>
2021-03-21 17:01:11 +0200

    Improved PostFeed

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,76 +1,31 @@
-import 'dayjs';
 import React from 'react';
-import Link from 'next/link';
-import { Pane, Avatar, Text, Heading } from 'evergreen-ui';
+import { Pane, Button, Spinner } from 'evergreen-ui';
+
 import { Post } from '../api/posts';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import PostItem from './PostItem';
 
 interface Props {
   admin?: boolean;
+  loading: boolean;
+  postsEnd: boolean;
   posts: Post[];
+  onLoadMore: () => Promise<void>;
 }
 
-interface PostItem {
-  post: Post;
-  admin: boolean;
-}
-
-const PostItem: React.FC<PostItem> = ({ post, admin = false }) => {
-  const date = new Date(post.createdAt);
-  dayjs.extend(relativeTime);
-
-  return (
-    <Pane
-      marginTop={12}
-      elevation={1}
-      width="60%"
-      minWidth={400}
-      padding={10}
-      display="flex"
-      border="muted"
-      alignItems="center"
-    >
-      <Avatar hashValue={post.uid} name=" " size={40} />
-      <Text marginX={20}>👍 {post.heartCount || 0} Hearts</Text>
-
-      <Pane>
-        <Link href={`/${post.username}/${post.slug}`}>
-          <Heading>
-            <a>{post.title}</a>
-          </Heading>
-        </Link>
-
-        <Text>{dayjs(date).fromNow()} • From {' '}</Text>
-
-        <Link href={`/${post.username}`}>
-          <Text cursor="pointer">
-            <strong>@{post.username}</strong>
-          </Text>
-        </Link>
-      </Pane>
-
-      {admin && (
-        <>
-          <Link href={`/admin/${post.slug}`}>
-            <h3>
-              <button>Edit</button>
-            </h3>
-          </Link>
-
-          {post.published ? <p>Live</p> : <p>Unpublished</p>}
-        </>
-      )}
-    </Pane>
-  );
-};
-
-const PostFeed: React.FC<Props> = ({ posts, admin }) => {
+const PostFeed: React.FC<Props> = ({ posts, loading, postsEnd, admin, onLoadMore }) => {
   return posts ? (
     <Pane flexDirection="column" display="flex" alignItems="center">
       {posts.map((post) => (
         <PostItem post={post} key={post.slug} admin={admin} />
       ))}
+
+      {!loading && !postsEnd && (
+        <Button marginTop={20} appearance="primary" intent="warning" onClick={onLoadMore}>
+          Load more
+        </Button>
+      )}
+
+      {loading && <Spinner marginY={20} />}
     </Pane>
   ) : null;
 };
---
+++ components/PostItem.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import 'dayjs';
+import dayjs from 'dayjs';
+import Link from 'next/link';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { Pane, Avatar, Text, Heading, Strong } from 'evergreen-ui';
+
+import { Post } from '../api/posts';
+
+interface Props {
+  post: Post;
+  admin: boolean;
+}
+
+const PostItem: React.FC<Props> = ({ post, admin }) => {
+  const date = new Date(post.createdAt);
+  dayjs.extend(relativeTime);
+
+  return (
+    <Link href={`/${post.username}/${post.slug}`}>
+      <Pane
+        cursor="pointer"
+        backgroundColor="white"
+        marginTop={15}
+        elevation={2}
+        width="60%"
+        minWidth={400}
+        padding={10}
+        display="flex"
+        border="muted"
+        alignItems="center"
+      >
+        <Avatar hashValue={post.slug} name=" " size={40} />
+
+        <Pane marginX={20}>
+          <Heading size={500} marginBottom={15}>
+            <a>{post.title}</a>
+          </Heading>
+          <Strong size={400}>👍 {post.heartCount || 0} • </Strong>
+
+          <Text>{dayjs(date).fromNow()} • From </Text>
+
+          <Link href={`/${post.username}`}>
+            <Text cursor="pointer">
+              <strong>@{post.username}</strong>
+            </Text>
+          </Link>
+        </Pane>
+
+        {admin && (
+          <>
+            <Link href={`/admin/${post.slug}`}>
+              <h3>
+                <button>Edit</button>
+              </h3>
+            </Link>
+
+            {post.published ? <p>Live</p> : <p>Unpublished</p>}
+          </>
+        )}
+      </Pane>
+    </Link>
+  );
+};
+
+export default PostItem;
